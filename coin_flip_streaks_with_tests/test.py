128dfd498b43e32ca1f2f1bd15cc856032947970
Lau <malaura.zam.pa@gmail.com>
2025-02-08 00:04:43 -0500

    Structure for coin flip exercise.

---
+++ coin_flip_streaks_with_tests/test.py
@@ -0,0 +1,16 @@
+import coinFlipStreaks as cfs
+
+def isTrue(expected, result):
+    if result == expected:
+        print("Test passed")
+        return
+    else:
+        print("Test failed")
+        print("Expected: " + expected + "  Result: " + result)
+        return
+
+  
+
+# def main():
+
+# main()
