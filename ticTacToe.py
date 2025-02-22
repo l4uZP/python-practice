aa65741da99491dc2eec35e103d6de6a7bedb7ed
Lau <malaura.zam.pa@gmail.com>
2025-02-21 22:12:37 -0500

    Finish tic tac toe game.

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 board = {"topL":" ", "topM":" ", "topR":" ", "midL":" ", "midM":" ", "midR":" ", "lowL":" ", "lowM":" ", "lowR":" "}
 keepPlaying = True
@@ -11,8 +12,8 @@ def printBoard():
     print("---------")
     print(board["lowL"] + " | " + board["lowM"] + " | " + board["lowR"])
 
-def printPositions():
-    print("It's your turn")
+def printInstructions():
+    print("\nIt's your turn")
     print("Choose your position: ")
     print("\ntopL, topM, topR")
     print("midL, midM, midR")
@@ -34,15 +35,61 @@ def isAvailable(move):
     else:
         return False
     
-def computerPlay():
+def computerPlaying():
+    print("\nComputer turn")
+    time.sleep(0.5)
+    print("\n...\n")
+    time.sleep(1)
+    
     while True:
         positions = list(board.keys())
         computerMove = positions[random.randint(0,len(positions)-1)]
+
         if isAvailable(computerMove):
             board[computerMove] = "o"
             break
 
-# def userPlay():
+        if noMovesLeft():
+            finishGame()
+            break
+
+def noMovesLeft():
+    if " " not in board.values():
+        print("Draw!")
+        return True
+    return False
+
+def userPlaying():
+    while True:
+        userMove = input()
+
+        if isAvailable(userMove):
+            board[userMove] = "x"
+            break
+        else:
+            print("Space not available. Try again.")
+
+def checkLine(pos1, pos2, pos3):
+    if board[pos1] == board[pos2] == board[pos3] != " ":
+        finishGame()
+        print(turn + " wins")
+
+
+def checkBoard():
+    checkLine("topL","topM","topR")    
+    checkLine("midL","midM","midR")
+    checkLine("lowL","lowM","lowR")
+    checkLine("topL","midL","lowL")
+    checkLine("topM","midM","lowM")
+    checkLine("topR","midR","lowR")
+    checkLine("topL","midM","lowR")
+    checkLine("topR","midM","lowL")
+        
+
+def finishGame():
+    global keepPlaying
+    keepPlaying = False
+    print("Game Over")
 
 def main():
     print("Start Game")
@@ -51,21 +98,13 @@ def main():
     while keepPlaying:
         global turn
         if turn == "user":
-            printPositions()
-
-            userMove = input()
-
-            if isAvailable(userMove):
-                board[userMove] = "x"
-            else:
-                print("Space not available. Try again.")
-                continue
-
+            printInstructions()
+            userPlaying()
         else:
-            print("Computer turn")
-
-            computerPlay()
+            computerPlaying()
 
         printBoard()
+        checkBoard()
         switchTurn()
+
 main()
